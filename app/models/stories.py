78266a2e0cafa7f7b2c0ab0248d38d6b18b78d02
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-07-06 15:38:45 -0700

    added stories model

---
+++ app/models/stories.py
@@ -0,0 +1,28 @@
+from .db import db
+
+class Story(db.Model):
+    __tablename__ = "stories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(50), nullable=False)
+    story = db.Column(db.Text(5000), nullable=False)
+    img = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    # FOR CASCADE DELETE
+    # db.Column(db.Integer, db.ForeignKey('bills.id', ondelete="CASCADE"))
+
+    comments = db.relationship("Comment", backpopulates="stories", cascade="all, delete")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "title": self.title,
+            "story": self.story,
+            "img": self.img,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+    }

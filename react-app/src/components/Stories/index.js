a39feb53b59dc2b15d5c29a933d003adf986255b
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-07-07 16:05:09 -0700

    styled the stories page a little

--- react-app/src/components/Stories/index.js
+++ react-app/src/components/Stories/index.js
@@ -1,33 +1,39 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { viewStories } from '../../store/stories.js'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { viewStories } from "../../store/stories.js";
+import "./Stores.css";
 
 function Stories() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const stories = useSelector((state) => {
-        return Object.values(state.stories)
-    });
+  const stories = useSelector((state) => {
+    return Object.values(state.stories);
+  });
 
-    useEffect(() => {
-        dispatch(viewStories());
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(viewStories());
+  }, [dispatch]);
 
-
-    return (
-        <>
-            <p>This is the feed of stories</p>
-            {stories.map((story) => (
-                <ul key={story.id}>
-                    <li>{story.title}</li>
-                    <li>{story.story}</li>
-                    <li>{story.user_id}</li>
-                    <li>{story.img}</li>
-                    <li>{story.created_at}</li>
-                </ul>
-            ))}
-        </>
-    )
+  return (
+    <>
+      <p>This is the feed of stories</p>
+      {stories.map((story) => (
+        <ul key={story.id}>
+        <div className="story-user-and-date-div">
+          <div>{story.user_id}</div>
+          <div>{story.created_at}</div>
+          </div>
+          <div className="each-story-div">
+            <div className="story-title-and-story-div">
+              <div className="story-title">{story.title}</div>
+              <div className="story-story">{story.story}</div>
+            </div>
+            <img src={story.img} className="story-img" />
+          </div>
+        </ul>
+      ))}
+    </>
+  );
 }
 
 export default Stories;

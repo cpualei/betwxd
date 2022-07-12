da66ed8e0f73eb62eeb659d5ffe1a0e9c363fffe
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-07-11 20:38:23 -0700

    styled side nav bar

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { logout } from '../../store/session';
+import "../NavBar.css"
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
   const history = useHistory()
-  
+
   const onLogout = async (e) => {
     await dispatch(logout());
     history.push('/')
   };
 
-  return <button onClick={onLogout}>Logout</button>;
+  return <button id="logout-btn" onClick={onLogout}>Sign out</button>;
 };
 
 export default LogoutButton;

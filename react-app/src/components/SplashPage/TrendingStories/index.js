5376580fb8a62785937d27e77ef890e90bfe5b38
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-07-15 20:36:49 -0700

    styled trending stories

--- react-app/src/components/SplashPage/TrendingStories/index.js
+++ react-app/src/components/SplashPage/TrendingStories/index.js
@@ -4,33 +4,35 @@ import { viewStories } from "../../../store/stories";
 import GetUser from "../../GetUser";
 import ReactTimeAgo from "react-time-ago";
 import trending from "../../../icons/trending.png";
-import "./TrendingStories.css"
+import "./TrendingStories.css";
 
 function TrendingStories() {
   const trendingStories = useSelector((state) => {
     return Object.values(state.stories);
   });
 
+  const numbers = ["01", "02", "03", "04", "05", "06"];
+
   return (
-    <>
-      <div className="trending-stories-container">
-        {/* <div className="trending-imgs"> */}
-          <img className="trending-imgs" src={trending} alt="trending" />
-        {/* </div> */}
+    <div className="trending-stories-container">
+      <div className="trending-stories-header">
+        <img className="trending-icon" src={trending} alt="trending" />
         <div className="trending-on-medium-text">TRENDING ON MEDIUM</div>
       </div>
-      {trendingStories.map((story) => (
-        <ul key={story?.id}>
-          <div>
-            <GetUser userId={story?.user_id} />
-          </div>
-          <div>{story.title}</div>
-          <div>
-            <ReactTimeAgo date={story?.created_at} locale="en-US" />
-          </div>
-        </ul>
-      ))}
-    </>
+      <div className="trending-ul-div">
+        {trendingStories.map((story) => (
+          <ul className="trending-ul" key={story?.id}>
+            <div>
+              <GetUser userId={story?.user_id} />
+            </div>
+            <div>{story.title}</div>
+            <div>
+              <ReactTimeAgo date={story?.created_at} locale="en-US" />
+            </div>
+          </ul>
+        ))}
+      </div>
+    </div>
   );
 }
 

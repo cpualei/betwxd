49d6e6d45e3604e3ed7241f485691c6a2e2be236
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-07-15 21:26:06 -0700

    styled trending stories, adjusted margins

--- react-app/src/components/SplashPage/TrendingStories/index.js
+++ react-app/src/components/SplashPage/TrendingStories/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 import { viewStories } from "../../../store/stories";
 import GetUser from "../../GetUser";
 import ReactTimeAgo from "react-time-ago";
@@ -11,7 +12,9 @@ function TrendingStories() {
     return Object.values(state.stories);
   });
 
-  const numbers = ["01", "02", "03", "04", "05", "06"];
+  const reversedTrending = trendingStories.reverse();
+
+//   const numbers = ["01", "02", "03", "04", "05", "06"];
 
   return (
     <div className="trending-stories-container">
@@ -20,12 +23,15 @@ function TrendingStories() {
         <div className="trending-on-medium-text">TRENDING ON MEDIUM</div>
       </div>
       <div className="trending-ul-div">
-        {trendingStories.map((story) => (
+        {reversedTrending.map((story) => (
           <ul className="trending-ul" key={story?.id}>
             <div>
+                <img id="trending-imgs" src={story.img} />
               <GetUser userId={story?.user_id} />
             </div>
-            <div>{story.title}</div>
+            <NavLink style={{textDecoration: "none", color: "rgba(41, 41, 41, 1)"}} to={`/stories/${story.id}`}>
+            <div id="trending-story-title">{story.title}</div>
+            </NavLink>
             <div>
               <ReactTimeAgo date={story?.created_at} locale="en-US" />
             </div>

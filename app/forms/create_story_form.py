5f7227df361c5a4f590a6981532344af2f40cdab
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-07-16 17:56:56 -0700

    fixed bug in create story.

--- app/forms/create_story_form.py
+++ app/forms/create_story_form.py
@@ -8,7 +8,7 @@ class CreateStoryForm(FlaskForm):
     user_id = IntegerField('User ID')
     title = TextAreaField('Title', validators=[DataRequired("*Please provide a title for your story."), Length(max=100, message="*Title must not exceed 100 characters.")])
     story = TextAreaField('Story', validators=[DataRequired("*Please provide a story to publish."), Length(max=100, message="*Story must not exceed 5000 characters.")])
-    img = StringField('Image', validators=[DataRequired("*Please provide an image in PNG, JPG or JPEG format."), Regexp("/(https?:\/\/.*\.(?:png|jpg|jpeg))/i", message="*Please provide an image in PNG, JPG or JPEG format.")])
+    img = StringField('Image', validators=[DataRequired("*Please provide an image in PNG, JPG or JPEG format."), Regexp("([^\\s]+(\\.(?i)(jpe?g|png|gif|bmp))$)", message="*Please provide an image in PNG, JPG or JPEG format.")])
     created_at = DateTime('Created at')
     updated_at = DateTime('Updated at')
     submit = SubmitField('Submit')

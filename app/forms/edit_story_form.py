ced3ffc1aefcbe607029bc8a8d0a1e118ead6f77
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-07-16 18:04:50 -0700

    fixed bug in edit story

--- app/forms/edit_story_form.py
+++ app/forms/edit_story_form.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField, SubmitField
 from sqlalchemy import DateTime
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Regexp
 
 
 class EditStoryForm(FlaskForm):
     user_id = IntegerField('User ID')
-    title = StringField('Title', validators=[DataRequired("*Please provide a title for your story.")])
-    story = StringField('Story', validators=[DataRequired("*Please provide a story to publish.")])
-    img = StringField('Image', validators=[DataRequired("*Please provide an image in PNG, JPG or JPEG format.")])
+    title = StringField('Title', validators=[DataRequired("*Please provide a title for your story."), Length(max=100, message="*Title must not exceed 100 characters.")])
+    story = StringField('Story', validators=[DataRequired("*Please provide a story for your post."), Length(max=5000, message="*Story must not exceed 5000 characters.")])
+    img = StringField('Image', validators=[DataRequired("*Please provide an image in PNG, JPG or JPEG format."), Regexp("([^\\s]+(\\.(?i)(jpe?g|png))$)", message="*Please provide an image in PNG, JPG or JPEG format.")])
     # created_at = DateTime('Created at')
     updated_at = DateTime('Updated at')
     submit = SubmitField('Submit')

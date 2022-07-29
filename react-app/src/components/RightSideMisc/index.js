c562b635740850163750a481d2be57895a6157ec
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-07-29 02:18:59 -0700

    added a 'check out their stories' section and included a list of users to view

--- react-app/src/components/RightSideMisc/index.js
+++ react-app/src/components/RightSideMisc/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "../SearchBar";
 import WhatWereReading from "./WhatWereReading";
 import RecommendedTopics from "./RecommendedTopics";
+import UsersList from "../UsersList";
 import "./RightSideMisc.css";
 
 function RightSideMisc() {
@@ -13,6 +14,7 @@ function RightSideMisc() {
       <div className="what-were-reading-recommended-container">
         <WhatWereReading />
         <RecommendedTopics />
+        <UsersList />
       </div>
     </div>
   );

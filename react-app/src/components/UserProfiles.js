c717c8b100625c2234349389a9f9864f62e2f42a
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-07-28 23:40:17 -0700

    added recommended topics component to user profile

--- react-app/src/components/UserProfiles.js
+++ react-app/src/components/UserProfiles.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import UsersStories from "./UsersStories";
+import RecommendedTopics from "./RightSideMisc/RecommendedTopics"
 import ProfileRightSideMisc from "./ProfileRightSideMisc";
 import "./UserProfiles.css";
 
@@ -46,6 +47,7 @@ function User() {
           <p id="profile-username">{user.username}</p>
           <p id="profile-bio">{user.bio}</p>
           {/* {sessionUser.id === user.id ? <p id="profile-edit-link">Edit profile</p> : null} */}
+        <RecommendedTopics />
         </div>
       </div>
     </div>

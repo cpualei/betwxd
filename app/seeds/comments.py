a7e1111b7e0ccd7c11420e25c24ef7aa89599853
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-07-29 00:42:28 -0700

    added seeder files to comments, stories, users

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -14,9 +14,9 @@ def seed_comments():
         created_at='2022-04-23',
         updated_at='2022-04-23')
     comment3 = Comment(
-        user_id=3,
+        user_id=4,
         story_id=3,
-        content='What a negative article. Elephant riding is fun.',
+        content="The vertebrae of the elephant's backbone point upwards just like humans. Instead of round and smooth discs, elephants have sharp, bony protrusions that extend upwards from their spine. If people constantly sit in metal seats on the back of the elephant, the animal feels pain and its health is severely damaged.",
         created_at='2022-04-23',
         updated_at='2022-04-23')
     comment4 = Comment(
@@ -31,12 +31,26 @@ def seed_comments():
         content='That is too bad. Onions taste amazing!',
         created_at='2022-07-08',
         updated_at='2022-07-08')
+    comment6 = Comment(
+        user_id=5,
+        story_id=4,
+        content='Sharks are the white blood cells of the ocean.',
+        created_at='2022-07-08',
+        updated_at='2022-07-08')
+    comment7 = Comment(
+        user_id=4,
+        story_id=4,
+        content='Killer whales eat great white sharks for breakfast.',
+        created_at='2022-07-08',
+        updated_at='2022-07-08')
 
     db.session.add(comment1)
     db.session.add(comment2)
     db.session.add(comment3)
     db.session.add(comment4)
     db.session.add(comment5)
+    db.session.add(comment6)
+    db.session.add(comment7)
 
     db.session.commit()
 
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -6,13 +6,13 @@ def seed_users():
     Demo = User(
         username='Demo', email='demo@aa.io', password='password', profile_photo='https://seeklogo.com/images/M/medium-2020-new-icon-logo-454E46D050-seeklogo.com.png', bio='Welcome to the demo profile page. Have fun demo-ing!')
     Marnie = User(
-        username='Marnie', email='marnie@aa.io', password='password', profile_photo='https://abovethelaw.com/wp-content/uploads/2017/04/Marnie-Michaels-Girls-300x198.jpg', bio='Lover of water, despiser or mosquitos')
+        username='Marnie', email='marnie@aa.io', password='password', profile_photo='https://abovethelaw.com/wp-content/uploads/2017/04/Marnie-Michaels-Girls-300x198.jpg', bio='Lover of water, despiser of mosquitos')
     Bobbie = User(
-        username='Bobbie', email='bobbie@aa.io', password='password', profile_photo='https://upload.wikimedia.org/wikipedia/commons/b/b9/Bobby_Cannavale_2009.jpg', bio='I am an animal advocator - a voice for those that do not have one.')
+        username='Bobbie', email='bobbie@aa.io', password='password', profile_photo='https://upload.wikimedia.org/wikipedia/commons/b/b9/Bobby_Cannavale_2009.jpg', bio='I am an animal advocate - a voice for those that do not have one.')
     Rihanna = User(
         username='Rihanna', email='rihanna@aa.io', password='password', profile_photo='https://assets.teenvogue.com/photos/604b63245a8c47a0fc860da0/4:3/w_2332,h_1749,c_limit/rihanna-lede.jpg', bio='You already know who I am.')
     Caitlin = User(
-        username='Caitlin', email='caitlin@aa.io', password='password', profile_photo='https://caitlinbuenlucas.com/assets/img/portfolio/me.png', bio='Welcome! Hope you are enjoying my full-stack pixel-perfect clone of Medium. Thanks for stopping by!')
+        username='Caitlin', email='caitlin@aa.io', password='password', profile_photo='https://caitlinbuenlucas.com/assets/img/portfolio/me.png', bio='Welcome to my full-stack pixel-perfect clone of Medium. Thanks for passing through.')
 
     db.session.add(Demo)
     db.session.add(Marnie)

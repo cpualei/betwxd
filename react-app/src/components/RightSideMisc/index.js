db06136cecc96e4c91487f49b18d9dfeaf626c80
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-07-28 21:57:31 -0700

    added recommended topics to right side div

--- react-app/src/components/RightSideMisc/index.js
+++ react-app/src/components/RightSideMisc/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchBar from "../SearchBar";
+import RecommendedTopics from "./RecommendedTopics";
 import greendot from "../../icons/greendot.png";
 import "./RightSideMisc.css";
 
@@ -9,9 +10,16 @@ function RightSideMisc() {
       <div className="stories-misc-div">
         <SearchBar />
       </div>
-      <div className="what-were-reading-today-div">
-        <img id="green-dot" src={greendot} />
-        <p id="what-were-reading-today">What We're Reading Today</p>
+      <div className="what-were-reading-recommended-container">
+        <div className="what-were-reading-today-div">
+          <img id="green-dot" src={greendot} />
+          <p id="what-were-reading-today">What We're Reading Today</p>
+        </div>
+        {/* <div className="recommended-topics-div"> */}
+          {/* <div className="re"> */}
+          <RecommendedTopics />
+          {/* </div> */}
+        {/* </div> */}
       </div>
     </div>
   );
